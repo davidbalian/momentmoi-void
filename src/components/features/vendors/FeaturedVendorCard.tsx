b04693e501d2f6d208766b88b4127d91bc43f2fc
MIR TAUHIDUL ISLAM <mislam.tauhidul@gmail.com>
2025-09-04 17:54:14 +0600

    Revert "initial codebase"
    This reverts commit 17cff5f0deff43752e0b51ba86e5a0cbd46ebc0b.

--- src/components/features/vendors/FeaturedVendorCard.tsx
+++ src/components/features/vendors/FeaturedVendorCard.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import React from "react";
+import Link from "next/link";
 import { Badge } from "@/components/ui/Badge";
 import { Icon } from "@/components/ui/Icon";
-import Link from "next/link";
 
 interface FeaturedVendor {
   id: string;
@@ -31,10 +32,7 @@ export function FeaturedVendorCard({ vendor }: FeaturedVendorCardProps) {
         {/* Background Image */}
         <div
           className="absolute inset-0 bg-cover bg-center transition-transform duration-300 group-hover:scale-110"
-          // style={{ backgroundImage: `url(${featuredImage})` }}
-          style={{
-            backgroundImage: `url(${"https://picsum.photos/seed/picsum/300/300"})`,
-          }}
+          style={{ backgroundImage: `url(${featuredImage})` }}
         />
 
         {/* Overlay for better text readability */}
